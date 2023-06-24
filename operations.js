7d3a656de126e642a747a11526eb50c0d2cbc1fe
André Sousa <107645540+andrelzsousa@users.noreply.github.com>
2023-06-24 16:06:55 -0300

    chained operations

--- operations.js
+++ operations.js
@@ -3,39 +3,59 @@ let newOperatorKey = false
 let clickOp = 0
 let firstOperator
 let secondOperator
+let result
 
 Array.from(operations).forEach((element) => {
     element.addEventListener('click', () => {
 
         if (clickOp == 0) {
-            firstOperator = getOperator()
-            newOperatorKey = true
-            clickOp++
-            opType = element.innerHTML
+
+
+            if (element.innerHTML == '=') {
+                result = makeOperation(result, secondOperator, opType)
+                setDisplay(result)
+            }
+            else {
+                firstOperator = getOperator()
+                newOperatorKey = true
+                clickOp++
+                opType = element.innerHTML
+            }
+
+
         }
         else if (clickOp == 1) {
             secondOperator = getOperator()
-            clickOp = 0
 
-            let result = makeOperation(firstOperator, secondOperator, opType)
+            result = makeOperation(firstOperator, secondOperator, opType)
             setDisplay(result)
-            newOperatorKey = true
 
-            //console.log(`${firstOperator} ${opType} ${secondOperator} = ${result}`)
+            firstOperator = result
+
+            if (element.innerHTML == '=') {
+                clickOp = 0
+            }
+            newOperatorKey = true
         }
     })
 })
 
 function getOperator() {
+    if (getDisplay() == "") {
+        return "0"
+    }
     return getDisplay()
 }
 
 function makeOperation(a, b, type) {
 
-    a = (a.includes(".")) ? parseFloat(a) : parseInt(a)
-    b = (b.includes(".")) ? parseFloat(b) : parseInt(b)
-    console.log(a, b)
-    console.log(type)
+    if (typeof (a) == "string") {
+        a = (a.includes(".")) ? parseFloat(a) : parseInt(a)
+    }
+    if (typeof (b) == "string") {
+        b = (b.includes(".")) ? parseFloat(b) : parseInt(b)
+    }
+
     switch (type) {
         case "+":
             return a + b
